ca4faaf35899ea9f234d56fc32f5e4bd2628d16e
Robin Appelman <robin@icewind.nl>
2017-02-16 14:28:05 +0100

    only load when in the files app
    Fixes #37

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,4 +1,9 @@
 <?php
-//load the required files
-OCP\Util::addscript( 'files_markdown', 'editor');
-OCP\Util::addStyle( 'files_markdown', 'preview' );
+$eventDispatcher = \OC::$server->getEventDispatcher();
+$eventDispatcher->addListener(
+	'OCA\Files::loadAdditionalScripts',
+	function () {
+		//load the required files
+		OCP\Util::addscript('files_markdown', 'editor');
+		OCP\Util::addStyle('files_markdown', 'preview');
+	});
