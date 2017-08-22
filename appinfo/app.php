d4ce7b0d27dd53d0538db21f3c59d7b989371fc6
Robin Appelman <robin@icewind.nl>
2017-08-22 18:35:28 +0200

    allow loading images from external sources
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,6 +3,10 @@ $eventDispatcher = \OC::$server->getEventDispatcher();
 $eventDispatcher->addListener(
 	'OCA\Files::loadAdditionalScripts',
 	function () {
+		$policy = new \OC\Security\CSP\ContentSecurityPolicy();
+		$policy->setAllowedImageDomains(['*']);
+		\OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
+		
 		//load the required files
 		OCP\Util::addscript('files_markdown', 'editor');
 		OCP\Util::addStyle('files_markdown', 'preview');
